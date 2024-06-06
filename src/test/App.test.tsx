5a9e7c66bed2ff9cb4b543d2d63b27aae06c16d1
colterlevi <cllongshore@gmail.com>
2024-06-06 10:23:22 -0400

    more tests

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,6 +1,10 @@
 import { test, expectTypeOf } from 'vitest'
 import Quiz from '../components/Quiz'
+import Home from '../components/Home'
+import Leaderboard from '../components/Leaderboard'
 
 test('my types work properly', () => {
     expectTypeOf(Quiz).toBeFunction()
+    expectTypeOf(Home).toBeFunction()
+    expectTypeOf(Leaderboard).toBeFunction()
 })
